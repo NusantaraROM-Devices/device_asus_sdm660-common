86cc83c6037e760bfee7a40c199cda399e50a57b
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    sdm660-common: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: Ieb69a8c8098b7ac236338b3619d2c6cf963b82fc

--- sdm660.mk
+++ sdm660.mk
@@ -121,7 +121,11 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.frameworks.displayservice@1.0
 
-# Doze mode
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
+# DPM
 PRODUCT_PACKAGES += \
     Doze
 
