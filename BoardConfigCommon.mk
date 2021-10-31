c8e47e0b163bd8d88ecd36490f78b7f80c257e09
harshhaareddy <harshhaa03@gmail.com>
2021-10-31 15:35:18 +0530

    Revert "[TMP] sdm660-common: Add minimal sepolicy"
    This reverts commit 51732827c023c4fc6e72fbf314a94f50b50fa4d7.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,11 +193,12 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy-minimal
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
 
 # Treble
 BOARD_VNDK_VERSION := current
