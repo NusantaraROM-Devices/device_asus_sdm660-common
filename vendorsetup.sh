dac33a67abd4d78b8b91a57c2673d8f6bdb15ed4
harshhaareddy <harshhaa03@gmail.com>
2022-07-10 15:13:42 +0530

    sdm660-common: vendorsetup.sh: Track Hals from Arrow Org
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,11 @@
 # Clone Required Hals
+#Hals
 rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/RONIN808/hardware_qcom-caf_msm8998_audio-1.git -b twelve hardware/qcom-caf/msm8998/audio
-rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_media.git -b twelve hardware/qcom-caf/msm8998/media
 rm -rf hardware/qcom-caf/msm8998/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_display.git -b twelve hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-12.0-caf-msm8998 hardware/qcom-caf/msm8998/media
+
+#GCamGo
+git clone https://github.com/Gaurav241/packages_apps_GoogleCamera.git -b twelve packages/apps/GoogleCam
