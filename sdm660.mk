10196645d109e944bb94a0ba8c7861cc1ad62e0c
harshhaareddy <harshhaa03@gmail.com>
2022-08-09 11:10:44 +0530

    sdm660-common: Move surfaceflinger props to vendor.prop
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -430,12 +430,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
-
 # Sqlite
 PRODUCT_PACKAGES += \
     sqlite3
